9950162d7b27f28344268b51d865d873f34dbdc4
RitaHuang9 <rr86916@gmail.com>
2023-05-20 12:07:43 +0800

    add: 作品上稿

--- src/views/work/WorkList.vue
+++ src/views/work/WorkList.vue
@@ -13,8 +13,21 @@
           </li>
         </ul>
       </div>
-      <div class="workList">
-        <a href="#" class="workList-wrapper" target="_blank">
+      <div class="workList" v-for="item in workList" :key="item.id">
+        <a :href="item.href" class="workList-wrapper" target="_blank">
+          <div class="workList-card">
+            <img class="workList-img" :src="item.imageName" alt="" />
+            <div class="workList-content">
+              <h3 class="workList-title">{{ item.title }}</h3>
+              <h4 class="workList-use">{{ item.tag }}</h4>
+              <h5 class="workList-txt">{{ item.content }}</h5>
+              <button type="button" class="btn">
+                <p>MORE</p>
+              </button>
+            </div>
+          </div>
+        </a>
+        <!-- <a href="#" class="workList-wrapper" target="_blank">
           <div class="workList-card">
             <img class="workList-img" src="@/assets/images/work/work-home.png" alt="" />
             <div class="workList-content">
@@ -22,24 +35,11 @@
               <h4 class="workList-use">VUE・SCSS</h4>
               <h5 class="workList-txt">說明說明說明說明說明說明說明說明說明</h5>
               <button type="button" class="btn">
-              <p>MORE</p>
-            </button>
+                <p>MORE</p>
+              </button>
             </div>
           </div>
-        </a>
-        <a href="#" class="workList-wrapper" target="_blank">
-          <div class="workList-card">
-            <img class="workList-img" src="@/assets/images/work/work-home.png" alt="" />
-            <div class="workList-content">
-              <h3 class="workList-title">MOONLIGHT</h3>
-              <h4 class="workList-use">VUE・SCSS</h4>
-              <h5 class="workList-txt">說明說明說明說明說明說明說明說明說明</h5>
-              <button type="button" class="btn">
-              <p>MORE</p>
-            </button>
-            </div>
-          </div>
-        </a>
+        </a> -->
       </div>
       <router-view></router-view>
     </div>
@@ -50,6 +50,23 @@
 import MainHeader from '../../components/MainHeader.vue'
 
 export default {
+  data() {
+    return {
+      imagePath:'/my-portfolio/src/assets/images/work/',
+      workList: [
+        {
+          category: 'F2E',
+          title: 'MOONLIGHT',
+          content: '說明說明說明說明說明說明說明說明說明',
+          tag: 'VUE・SCSS',
+          id: '001',
+          imageName: import.meta.env.VITE_PATH + 'work-home.png',
+          imageInfo:'MOONLIGHT示意圖',
+          href: 'https://ritahuang9.github.io/Home-furniture/#/user/index',
+        }
+      ]
+    }
+  },
   components: {
     MainHeader
   }
